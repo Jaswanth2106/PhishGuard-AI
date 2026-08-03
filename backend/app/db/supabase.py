ec56e0f795ee429f3887f7405d0d68cfde6bcc97
Jaswanth Naidu Nainala <jaswanthnaidunainala@gmail.com>
2026-08-03 13:13:35 +0530

    chore: Remove sensitive SUPABASE key from logs

--- backend/app/db/supabase.py
+++ backend/app/db/supabase.py
@@ -2,7 +2,6 @@ from supabase import create_client, Client
 from app.core.config import settings
 
 print("URL:", settings.SUPABASE_URL)
-print("KEY:", settings.SUPABASE_SERVICE_ROLE_KEY[:30])
 
 supabase: Client = create_client(
     settings.SUPABASE_URL,
